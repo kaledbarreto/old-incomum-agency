9d0946e49af89c7073d1bbb8e326b65d0eba8107
Kaled Barreto <kaledbarreto@gmail.com>
2022-03-28 14:02:45 -0300

    [NEW] Creating and Clean React Project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function Home() {
+
+  return (
+    <>
+      <h1>Agência Incomum</h1>
+    </>
+  )
+}
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+
+function Routes() {
+  return (
+    <div className="routes">
+      <Home />
+    </div>
+  );
+}
+
+export default Routes;
