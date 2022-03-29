8766090e8154c48dc1872be7f5f013bce3250ac4
Kaled Barreto <kaledbarreto@gmail.com>
2022-03-29 10:57:26 -0300

    [NEW] Add Navbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,23 @@ function Home() {
 
   return (
     <>
-      <h1>Agência Incomum</h1>
+      <div className="navbar">
+        <h1>Agência Incomum</h1>
+        <div className="navlist">
+          <li>
+            Home
+          </li>
+          <li>
+            Princípios e Valores
+          </li>
+          <li>
+            O que fazemos
+          </li>
+          <li>
+            Quem somos
+          </li>
+        </div>
+      </div>
     </>
   )
 }
